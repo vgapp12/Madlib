b45abdedc403c0d50043720e31db907c30de74f3
DePaul <CONF0586@depaul.edu>
2018-07-24 15:42:55 -0500

    commit message

---
+++ new 1.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+	console.log("ready");
+		var search = $("#first_name").val();
+		console.log(name);
+		
+	var a= adjective
+	var b= adverb
+	var c= adjective
+	var d= noun 
+	
+	console.log("hairy")=var a
+	
+		$("button").click( function() {
+		console.log("clicked!!");
+		$("body").css("generate sentence");
+	
+		
+
+
+		
+		
